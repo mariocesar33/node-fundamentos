8d62634318be7e4556b4f452efb864f1ed64f5a4
Mário César Silva <mcgato33@hotmail.com>
2023-01-31 10:58:21 -0100

    Atualização de registros

--- src/routes.js
+++ src/routes.js
@@ -43,5 +43,20 @@ export const routes = [
 
       return response.writeHead(204).end();
     }
+  },
+  {
+    method: "PUT",
+    path: buildRoutePath("/users/:id"),
+    handler: (request, response) => {
+      const { id } = request.params;
+      const { name, email } = request.body;
+
+      database.update("users", id, {
+        name,
+        email,
+      });
+
+      return response.writeHead(204).end();
+    }
   }
 ];
