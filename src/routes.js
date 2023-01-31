3cfd1d073bb04c70e53353dac43ff8726e04d175
Mário César Silva <mcgato33@hotmail.com>
2023-01-31 10:30:29 -0100

    Remoção de registros

--- src/routes.js
+++ src/routes.js
@@ -36,7 +36,12 @@ export const routes = [
     method: "DELETE",
     path: buildRoutePath("/users/:id"),
     handler: (request, response) => {
-      return response.end();
+      // console.log(request.params);
+      const { id } = request.params;
+
+      database.delete("users", id);
+
+      return response.writeHead(204).end();
     }
   }
 ];
